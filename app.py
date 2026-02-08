06971c9aa77c88d5567a03444cf01351b2b55a0c
tyalavender-sudo <tya.lavender@gmail.com>
2026-02-07 19:33:50 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ class User(db.Model):
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     courses = db.Column(db.String(200), nullable=False)
-    availability = db.Column(db.String(100), nullable=False)
-    location = db.Column(db.String(100))
+    day = db.Column(db.String(100), nullable=False)
+    time = db.Column(db.String(100), nullable=False)
     people = db.Column(db.Integer, nullable = False)
 
     def courses_list(self):
@@ -26,8 +26,8 @@ class User(db.Model):
         return courses_list
 
     def __repr__(self):
-        return (f"User('{self.username}', '{self.email}', '{self.courses}', '{self.availability}', "
-                f"'{self.location}', '{self.people}')")
+        return (f"User('{self.username}', '{self.email}', '{self.courses}', '{self.day}', "
+                f"'{self.time}', '{self.people}')")
 
 with app.app_context():
     db.create_all()
@@ -40,14 +40,62 @@ def submit():
     new = User(
         username=request.form.get('username'),
         email=request.form.get('email'),
-        courses=request.form.get('courses').lower(),
-        availability=request.form.get('availability'),
-        location=request.form.get('location'),
+        courses=request.form.get('courses', " ").lower(),
+        day=request.form.get('day'),
+        time=request.form.get('time'),
         people=int(request.form.get('people')))
+
 
     db.session.add(new)
     db.session.commit()
-    return "You create a new user successfully"
+    result = find_matches(new)
+    return render_template('results.html', user = new, result = result)
+
+def find_matches(new, n = None):
+    if n is None:
+        n = new.people
+    other_users = User.query.filter(User.id != new.id).all()
+    score_matches = []
+    new_courses_list = new.courses_list()
+
+    for user in other_users:
+        score = 0
+        other_courses = user.courses_list()
+
+        for course in other_courses:
+            if course.lower() in new_courses_list:
+                score += 10
+
+        if new.day == user.day:
+            score += 5
+
+        if new.time == user.time:
+            score += 3
+
+        if score > 0:
+            score_matches.append((user, score))
+
+    length = len(score_matches)
+    for i in range(length):
+        max_idx = i
+        for j in range(i + 1, length):
+            if score_matches[j][1] > score_matches[max_idx][1]:
+                max_idx = j
+
+        score_matches[i], score_matches[max_idx] = score_matches[max_idx], score_matches[i]
+
+    final_results = []
+
+    count = 0
+    for score1 in score_matches:
+        if count < n:
+            user_obj = score1[0]
+            final_results.append(user_obj)
+            count += 1
+        else:
+            break
+
+    return final_results
 
 if __name__ == '__main__':
     app.run(debug = True)
